2f01da27cba3e4c30838e78a844c1d4121c0c510
Brent <riva.brent@gmail.com>
2018-03-04 22:12:44 -0500

    WIP

--- bot.py
+++ bot.py
@@ -2,16 +2,52 @@ from telegram.ext import Updater, CommandHandler
 import telegram
 import logging
 
+# Steam Constants
+steamAPIKey = '55B92D3F605D8500F4FAC0C8FB21C685'
+steamMatchHistoryCall = 'https://api.steampowered.com/IDOTA2MATCH_570/GetMatchHistory/V001/?account_id={}&key=' + steamAPIKey
+
+#OpenDota Constants
+openDotaMatchInfoCall = 'https://api.opendota.com/api/players/{}/recentMatches'
 # One bot to rule them all
 bot = telegram.Bot(token='257069062:AAEuddnPDHuw5KlTLrL5eOiTEs9-xllqV9w')
 updater = Updater(token='257069062:AAEuddnPDHuw5KlTLrL5eOiTEs9-xllqV9w')
+dispatcher = updater.dispatcher
+feeders = {
+    "Brent": '3763200887',
+    "Chris": '290262710',
+    "Elijah": '146700431',
+    "Eric": '189426584',
+    "Hannah": '246783176',
+    "Luis": '27832643',
+    "Tucker": '364488795'
+    }
 
+feederDeaths = {
+    "Brent": 0,
+    "Chris": 0,
+    "Elijah": 0,
+    "Eric": 0,
+    "Hannah": 0,
+    "Luis": 0,
+    "Tucker": 0
+    }
+
+feederWorstMatchId = {
+    "Brent": 0,
+    "Chris": 0,
+    "Elijah": 0,
+    "Eric": 0,
+    "Hannah": 0,
+    "Luis": 0,
+    "Tucker": 0
+}
 # logging to diagnose my many issues. If only it worked in real life :v
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
+
 # print bot information. Good to know if we have a working bot.
-logger.info("Bot information: " + bot.getMe())
+bot.getMe()
 
 # Hello, world!
 def hello(bot, update):
@@ -21,11 +57,38 @@ def hello(bot, update):
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Beep Doot. You have awoken me.")
 
-    # start polling.
-    updater.start_polling()
-    updater.idle()
+def unknown(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-# run hello when somebody types 'hello'
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+# TIME TO START THE FEED PARSING
+def topFeeds(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Public humiliation is key to improvement.\n")
+
+    for feeder in feeders:
+        logger.info("Getting information for " + feeder)
+        resp = requests.get(openDotaMatchInfoCall.format(feeders[feeder]))
+
+        if resp.status_code != 200:
+            logger.error("Error getting player information | ID: " + feeder)
+            bot.send_message(chat_id=update.message.chat_id, text="Error getting player information | ID: " + feeder)
+
+        matches = resp.json()
+        logger.debug(matches)
+
+        # Add up deaths for the feeder
+        for match in matches:
+            localDeathMax = 0
+            feederDeaths[feeder] += match['deaths']
+
+            if match['deaths'] > localDeathMax:
+                localDeathMax = match['deaths']
+                feederWorstMatchId = match['match_id']
+
+
+# start polling.
+updater.start_polling()
+
+# dispatcher handlers.
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('hello', hello))
+dispatcher.add_handler(MessageHandler(Filters.command, unknown)), group=1)
